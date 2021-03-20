39588e5e991ecddcca9085f9e20b123096b1fcbb
GluhovDmitry <you@example.com>
2021-03-20 16:56:14 +0500

    Created conversion coroutine

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from aiohttp import web
+import json
+
+
+async def convert(request):
+    try:
+        response_obj = {'status': 'success'}
+
+        cur_from = request.query['from']
+        cur_to = request.query['to']
+        amount = request.query['amount']
+        print(cur_from, cur_to, amount)
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as e:
+        response_obj = {'status': 'failed', 'reason': str(e)}
+        return web.Response(text=json.dumps(response_obj), status=500)
 
 
 
+app = web.Application()
+#app.router.add_get('/', handle)
+routes = [web.get('/convert', convert)]
+app.add_routes(routes)
 
+host = '127.0.0.1'
+port = 5000
 
 if __name__ == '__main__':
-    pass
+    web.run_app(app, host=host, port=port)
 
 
