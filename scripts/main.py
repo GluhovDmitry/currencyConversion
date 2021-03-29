210cf2dafa4b30dd19ee864edbe243fb20940296
GluhovDmitry <you@example.com>
2021-03-29 21:59:51 +0500

    Refactored

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ async def init():
 
 
 if __name__ == '__main__':
-    host = '127.0.0.1'
+    host = '0.0.0.0'
     port = 5000
     application = init()
     web.run_app(application, host=host, port=port)
